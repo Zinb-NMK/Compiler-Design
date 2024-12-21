cd62accb34df2f4a79d29c9131527149060bb3b7
N.ManojKumar <nagarammanojkumar333@gmail.com>
2024-12-21 16:11:33 +0530

    Random Pass & User

--- CD-LAB-P/Practical 2.c
+++ CD-LAB-P/Practical 2.c
@@ -3,19 +3,26 @@
 
 int main() {
     char username[50], password[50];
-    char validUsername[] = "user123";
-    char validPassword[] = "pass123";  
-
-    printf("Enter username: ");
-    scanf("%s", username);
-    
-    printf("Enter password: ");
-    scanf("%s", password);
-    
-    if (strcmp(username, validUsername) == 0 && strcmp(password, validPassword) == 0) {
-        printf("Login successful!\n");
-    } else {
-        printf("Invalid username or password.\n");
+    char validUsername[] = "user"; 
+    char validPassword[] = "pass"; 
+    while (1) {
+        printf("Enter username: ");
+        scanf("%s", username);
+        
+        printf("Enter password: ");
+        scanf("%s", password);
+        
+        if (strlen(username) != 4 || strlen(password) != 4) {
+            printf("Both username and password must be exactly 4 characters long.\n");
+            continue; 
+        }
+        
+        if (strcmp(username, validUsername) == 0 && strcmp(password, validPassword) == 0) {
+            printf("Login successful!\n");
+            break;  
+        } else {
+            printf("Invalid username or password. Please try again.\n");
+        }
     }
 
     return 0;
